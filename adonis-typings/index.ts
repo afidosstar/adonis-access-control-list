e73a2105413931ecd8ca03a9c6949ac0b74fd76c
afidos <fiacre.ayedoun@gmail.com>
2022-10-18 19:09:54 +0100

    fix: delete auth interface

--- adonis-typings/index.ts
+++ adonis-typings/index.ts
@@ -9,6 +9,6 @@
  * file that was distributed with this source code.
  */
 
-/// <reference path="./auth.ts"/>
+//// <reference path="./auth.ts"/>
 /// <reference path="./router.ts"/>
 /// <reference path="./adonis-access-control-list.ts"/>
