7c556b621d952d6fcb384ab7345c34e63e236f3f
AYEDOUN Dossou Fiacre <fiacre.ayedoun@gmail.com>
2026-02-11 12:41:16 +0100

    fix(types): correction des définitions TypeScript et de la commande de test • Typage : Refonte de adonis-access-control-list.ts pour corriger les erreurs d'augmentation de module et déplacer les imports dans les déclarations.

--- adonis-typings/adonis-access-control-list.ts
+++ adonis-typings/adonis-access-control-list.ts
@@ -7,121 +7,112 @@
  * file that was distributed with this source code.
  */
 
-declare module "@ioc:Adonis/Addons/Acl/Models/Permission" {
-  import { LucidModel, LucidRow } from "@ioc:Adonis/Lucid/Orm";
-  import { DateTime } from "luxon";
-  import { SoftDeletes } from "@ioc:Adonis/Addons/LucidSoftDeletes";
+declare module '@ioc:Adonis/Addons/Acl/Models/Permission' {
+  import { LucidModel, LucidRow } from '@ioc:Adonis/Lucid/Orm'
+  import { DateTime } from 'luxon'
+  import { SoftDeletes } from '@ioc:Adonis/Addons/LucidSoftDeletes'
 
-  export interface PermissionInterface {
-    id: number;
-    name: string;
-    slug: string;
-    description: string;
-    route: string;
-    group: string;
-    createdAt: DateTime;
-    updatedAt: DateTime;
-    deletedAt?: DateTime;
+  export interface Permission extends LucidRow {
+    id: number
+    name: string
+    slug: string
+    description: string
+    route: string
+    group: string
+    createdAt: DateTime
+    updatedAt: DateTime
+    deletedAt?: DateTime
   }
 
-  export type PermissionModelType = LucidModel & {
-    new (...args: any[]): LucidRow & PermissionInterface;
-  } & typeof SoftDeletes;
-
-  const Permission: PermissionModelType;
-  export default Permission;
-}
-
-declare module "@ioc:Adonis/Addons/Acl/Models/Role" {
-  import { LucidModel, LucidRow, ManyToMany } from "@ioc:Adonis/Lucid/Orm";
-  import { DateTime } from "luxon";
-  import { SoftDeletes } from "@ioc:Adonis/Addons/LucidSoftDeletes";
-  import { PermissionModelType } from "@ioc:Adonis/Addons/Acl/Models/Permission";
-
-  export interface RoleInterface {
-    id: number;
-    name: string;
-    slug: string;
-    description: string;
-    permissions: ManyToMany<PermissionModelType>;
-    createdAt: DateTime;
-    updatedAt: DateTime;
-    deletedAt?: DateTime;
+  export interface PermissionModel extends LucidModel {
+    new (...args: any[]): Permission
   }
 
-  export type RoleModelType = LucidModel & {
-    new (...args: any[]): LucidRow & RoleInterface;
-  } & typeof SoftDeletes;
-
-  const Role: RoleModelType;
-  export default Role;
+  const Permission: PermissionModel & typeof SoftDeletes
+  export default Permission
 }
 
-declare module "@ioc:Adonis/Addons/Acl" {
-  import {
-    LucidModel,
-    LucidRow,
-    ManyToMany,
-    ColumnOptions,
-  } from "@ioc:Adonis/Lucid/Orm";
-  import { RouteMiddlewareHandler } from "@ioc:Adonis/Core/Route";
-  import { NormalizeConstructor } from "@poppinss/utils/build/src/Helpers";
-  import { PermissionModelType } from "@ioc:Adonis/Addons/Acl/Models/Permission";
-  import { RoleModelType } from "@ioc:Adonis/Addons/Acl/Models/Role";
+declare module '@ioc:Adonis/Addons/Acl/Models/Role' {
+  import { LucidModel, LucidRow, ManyToMany } from '@ioc:Adonis/Lucid/Orm'
+  import { DateTime } from 'luxon'
+  import { SoftDeletes } from '@ioc:Adonis/Addons/LucidSoftDeletes'
+  import { PermissionModel } from '@ioc:Adonis/Addons/Acl/Models/Permission'
 
-  export type AccessRouteContract = {
-    name: string;
-    description: string;
-    group?: string;
-  };
+  export interface Role extends LucidRow {
+    id: number
+    name: string
+    slug: string
+    description: string
+    permissions: ManyToMany<PermissionModel>
+    createdAt: DateTime
+    updatedAt: DateTime
+    deletedAt?: DateTime
+  }
+
+  export interface RoleModel extends LucidModel {
+    new (...args: any[]): Role
+  }
+
+  const Role: RoleModel & typeof SoftDeletes
+  export default Role
+}
+
+declare module '@ioc:Adonis/Addons/Acl' {
+  import { LucidModel, LucidRow, ManyToMany, ColumnOptions } from '@ioc:Adonis/Lucid/Orm'
+  import { RouteMiddlewareHandler } from '@ioc:Adonis/Core/Route'
+  import { NormalizeConstructor } from '@poppinss/utils/build/src/Helpers'
+  import { PermissionModel } from '@ioc:Adonis/Addons/Acl/Models/Permission'
+  import { RoleModel } from '@ioc:Adonis/Addons/Acl/Models/Role'
+
+  export interface AccessRouteContract {
+    name: string
+    description: string
+    group?: string
+  }
 
   export interface ConfigAclContract {
-    prefix?: string;
-    middlewares?: RouteMiddlewareHandler | RouteMiddlewareHandler[];
+    prefix?: string
+    middlewares?: RouteMiddlewareHandler | RouteMiddlewareHandler[]
     joinTables: {
-      permissionRole: string;
-      permissionUser: string;
-      userRole: string;
-    };
-    apiOnly: boolean;
-    superAdminRole?: string;
+      permissionRole: string
+      permissionUser: string
+      userRole: string
+    }
+    apiOnly: boolean
+    superAdminRole?: string
   }
 
-  export type AclAuthDecorator = (target: LucidRow, property: string) => void;
+  export type AclAuthDecorator = (target: LucidRow, property: string) => void
 
-  export type AclAuthUser = {
-    roles: ManyToMany<RoleModelType>;
-    permissions: ManyToMany<PermissionModelType>;
+  export interface AclAuthUser {
+    roles: ManyToMany<RoleModel>
+    permissions: ManyToMany<PermissionModel>
 
-    getAccesses(): Promise<string[]>;
-    can(slug: string): Promise<boolean>;
-    getRoles(): Promise<string[]>;
-    getPermissions(): Promise<string[]>;
+    getAccesses(): Promise<string[]>
+    can(slug: string): Promise<boolean>
+    getRoles(): Promise<string[]>
+    getPermissions(): Promise<string[]>
 
-    hasRole(slug: string): Promise<boolean>;
-    hasAnyRole(slugs: string[]): Promise<boolean>;
-    hasAllRoles(slugs: string[]): Promise<boolean>;
+    hasRole(slug: string): Promise<boolean>
+    hasAnyRole(slugs: string[]): Promise<boolean>
+    hasAllRoles(slugs: string[]): Promise<boolean>
 
-    hasPermission(slug: string): Promise<boolean>;
-    hasAnyPermission(slugs: string[]): Promise<boolean>;
-    hasAllPermissions(slugs: string[]): Promise<boolean>;
+    hasPermission(slug: string): Promise<boolean>
+    hasAnyPermission(slugs: string[]): Promise<boolean>
+    hasAllPermissions(slugs: string[]): Promise<boolean>
 
-    isSuperAdmin(): Promise<boolean>;
-    loadPermissions(): Promise<void>;
-  };
-
-  interface AuthUserFn {
-    (
-      options?: Partial<ColumnOptions & { isUpdated?: boolean }>
-    ): AclAuthDecorator;
+    isSuperAdmin(): Promise<boolean>
+    loadPermissions(): Promise<void>
   }
 
-  type ExtendUser = <T extends NormalizeConstructor<LucidModel>>(
-    superclass: T
-  ) => T & {
-    new (...args: any[]): AclAuthUser;
-  };
+  export interface AuthUserFn {
+    (options?: Partial<ColumnOptions & { isUpdated?: boolean }>): AclAuthDecorator
+  }
 
-  export const authUser: AuthUserFn;
-  export const BaseUser: ExtendUser;
+  export type ExtendUser = <T extends NormalizeConstructor<LucidModel>>(superclass: T) => T & {
+    new (...args: any[]): AclAuthUser
+  }
+
+  export const authUser: AuthUserFn
+  export const BaseUser: ExtendUser
 }
